c3964de89beabf13be7e6d07ae7be22b6d316f69
Robert <englishincourbevoie@gmail.com>
2023-01-25 17:44:20 +0100

    Non-functional commit; tags rendered as error codes

--- RobertCustomisations/RobertTest.py
+++ RobertCustomisations/RobertTest.py
@@ -1,7 +1,25 @@
 # Outdated packages now require special setup at start of script
+import sys
+import jinja2
+import markupsafe
+# workaround for setup
+jinja2.Markup = markupsafe.Markup
+jinja2.evalcontextfilter = jinja2.pass_context
 
-    import jinja2
-    import markupsafe
-    jinja2.Markup = markupsafe.Markup
-    jinja2.evalcontextfilter = jinja2.pass_context
-    import secretary
+from secretary import Renderer
+
+
+# Basic definitions
+template = sys.argv[1]
+
+
+engine = Renderer()
+engine.environment.variable_start_string  = '[['
+engine.environment.variable_end_string = ']]'
+engine.environment.block_start_string = '{{'
+engine.environment.block_end_string = '}}'
+
+result = engine.render(template, test="foo", test1="bar")
+
+output = open('rendered_document.odt', 'wb')
+output.write(result)
